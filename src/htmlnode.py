a8b22cef1df07fd3fcc1da9fb2a5ed421018cbb4
geekyfirebird <geekyfirebird@gmail.com>
2026-03-09 13:26:46 -0400

    Set up ParentNode and Test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,21 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode: {self.tag} {self.value} {self.props}"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Missing a tag")
+        if self.children is None:
+            raise ValueError("Missing value for children") 
+        child_string = ""
+        for each in self.children:
+            child_string += each.to_html()
+        full_string = f"<{self.tag}{self.props_to_html()}>{child_string}</{self.tag}>"
+        return full_string
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -25,3 +25,18 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_repr_a(self):
         node = LeafNode("a", "This is a new link", {"href": "https://www.google.com", "target": "_blank"})
         print(node)
+
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
