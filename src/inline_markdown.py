a9fb003dce475a0bf1257b4da0fbfaa064b90745
geekyfirebird <geekyfirebird@gmail.com>
2026-03-09 15:26:11 -0400

    Setup inline markdown and tests

---
+++ src/inline_markdown.py
@@ -0,0 +1,22 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_list = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_list.append(old_node)
+            continue
+        if old_node.text.count(delimiter) % 2 != 0:
+             raise Exception("Invalid Markdown Syntax") 
+        split_list = old_node.text.split(delimiter)
+        for i in range(len(split_list)):
+            if split_list[i] == "":
+                continue
+            if i == 0:
+                new_list.append(TextNode(split_list[i], TextType.TEXT))
+            elif i % 2:
+                new_list.append(TextNode(split_list[i], text_type))
+            else:
+                new_list.append(TextNode(split_list[i], TextType.TEXT))
+    return new_list
