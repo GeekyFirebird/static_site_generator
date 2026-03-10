43d8a3a3b9def878bf0296140a456d96d6e88146
geekyfirebird <geekyfirebird@gmail.com>
2026-03-10 16:30:26 -0400

    Update block markdown with block to block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,17 @@
+from enum import Enum
 import re
 
 from textnode import TextNode, TextType
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 
 def markdown_to_blocks(markdown):
     list_markdown = []
@@ -10,3 +21,29 @@ def markdown_to_blocks(markdown):
             continue
         list_markdown.append(each.strip())
     return list_markdown
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 from textnode import TextNode, TextType
 
@@ -24,6 +24,9 @@ This is the same paragraph on a new line
             ],
         )
 
+
+
+
     def test_markdown_to_blocks_newlines(self):
         md = """
 This is **bolded** paragraph
@@ -47,6 +50,20 @@ This is the same paragraph on a new line
             ],
         )
 
+
+    def test_block_to_block_types(self):
+            block = "# heading"
+            self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+            block = "```\ncode\n```"
+            self.assertEqual(block_to_block_type(block), BlockType.CODE)
+            block = "> quote\n> more quote"
+            self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+            block = "- list\n- items"
+            self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+            block = "1. list\n2. items"
+            self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+            block = "paragraph"
+            self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
