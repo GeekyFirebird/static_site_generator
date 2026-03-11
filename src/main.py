f9a0d3992348da1372e17a32dc6a2172ca58ab08
geekyfirebird <geekyfirebird@gmail.com>
2026-03-11 17:28:33 -0400

    Started working on recursive function for paths

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from textnode import TextNode, TextType
+import os, shutil
 
 def main():
     print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+
+def recursive_function(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    
 
 main()
 
